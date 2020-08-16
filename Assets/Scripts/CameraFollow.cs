1fd1053b1bb738d6373301ef2f6de41e43a03cca
Curtice Gough <31411535+Curtico@users.noreply.github.com>
2020-08-15 21:09:23 -0400

    Add files via upload

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float boundaryDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Right boundary
+        if (target.transform.position.x - transform.position.x > boundaryDistance)
+        {
+            transform.position = new Vector3(target.transform.position.x - boundaryDistance, transform.position.y, transform.position.z);
+        }
+
+        // Left boundary
+        if (target.transform.position.x - transform.position.x < boundaryDistance * -1)
+        {
+            transform.position = new Vector3(target.transform.position.x + boundaryDistance, transform.position.y, transform.position.z);
+        }
+
+        // Upper boundary
+        if (target.transform.position.y - transform.position.y > boundaryDistance)
+        {
+            transform.position = new Vector3(transform.position.x, target.transform.position.y - boundaryDistance, transform.position.z);
+        }
+
+        // Lower boundary
+        if (target.transform.position.y - transform.position.y < boundaryDistance * -1)
+        {
+            transform.position = new Vector3(transform.position.x, target.transform.position.y + boundaryDistance, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Left & Right movement
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0));
+    }
+
+    // FixedUpdate is used for Rigidbody2D interactions
+    private void FixedUpdate()
+    {
+        // Jumping
+        if (Input.GetKey("space") && Mathf.Abs(rb.velocity.y) < 0.1)
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        // do not allow physics rotation
+        rb.freezeRotation = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            transform.rotation = collision.gameObject.transform.rotation;
+        }
+    }
+}
